3ea0df8340b748cb640fb370f7d535a7f076f16d
ksyukaksyu <ksyukaksyu@gmail.com>
2018-04-08 22:19:08 +0300

    fixed questions and def get_different_friends

--- main.py
+++ main.py
@@ -2,26 +2,7 @@ import requests
 from time import sleep
 import json
 
-# ВОПРОС 1: нужно ли писать авторизацию? Или это только для приложения в вк?
-
-# AUTH_URL = 'https://oauth.vk.com/authorize?'
-#
-# params = {
-#     'client_id': '',
-#     'display': 'page',
-#     'redirect_uri': 'https://oauth.vk.com/blank.html',
-#     'scope': 'friends',
-#     'response_type': 'token',
-#     'v': '5.52',
-# }
-# auth = requests.get(AUTH_URL, params=params)
-# print(auth.url)
-
 TOKEN_VK = '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099'
-
-# ВОПРОС 2: в целом, хочу получить комментарий о логичности выбора именно такой структуры классов, мне кажется, что я
-# сделала не совсем верно и не максимально эффективно. Я постаралась максимально избежать дублирования кода, но опять
-# же, возможно, можно сделать лучше. Если да, подскажите, пожалуйста.
 
 
 class VKController:
@@ -29,10 +10,8 @@ class VKController:
         self.token = token
         self.version = '5.73'
 
-    # ВОПРОС 3: могут ли params быть по умолчанию пустыми? Pycharm ругается
-    def get_request(self, method_name, params = {}):
+    def get_request(self, method_name, params = None):
         sleep(0.3)
-        # ВОПРОС 4: пыталась сделать прогресс бар с помощью clint, и не поняла, как в этот код его правильно впихнуть?
         print('    Request {}... '.format(method_name))
         params['v'] = self.version
         params['access_token'] = self.token
@@ -44,8 +23,8 @@ class VKController:
     def get_request_response_items(self, method_name, params):
         return self.get_request(method_name, params)['response']['items']
 
-    # ВОПРОС 5: необходимо ли методы без self выносить за пределы класса?
-    def save_to_json(self, file_name, data):
+    @classmethod
+    def save_to_json(cls, file_name, data):
         with open('{}.json'.format(file_name), 'w') as f:
             json.dump(data, f, ensure_ascii = False, indent = 4)
         print('Список групп доступен в файле {}.json.'.format(file_name))
@@ -56,18 +35,20 @@ class VKUser(VKController):
         super().__init__(token)
         self.user_id = user
 
-    def get_unmutual_groups(self):
-        unmutual_groups = set()
+    def get_different_groups(self):
+        different_groups = set()
+        user_friends = set(self.get_request_response_items('friends.get', {
+            'user_ids': self.user_id
+        }))
         for group in self.get_request_response_items('groups.get', {
             'user_ids': self.user_id
         }):
-
-            if not self.get_request_response_items('groups.getMembers', {
-                'filter': 'friends',
+            group_members = set(self.get_request_response_items('groups.getMembers', {
                 'group_id': group
-            }):
-                unmutual_groups.add(str(group))
-        return unmutual_groups
+            }))
+            if not group_members & user_friends:
+                different_groups.add(str(group))
+        return different_groups
 
     def get_group_info(self, data):
         group_info_list = []
@@ -89,51 +70,7 @@ class VKUser(VKController):
 
 def main():
     user = VKUser(TOKEN_VK, input('Введите id или короткий адрес пользователя VK: '))
-    user.save_to_json('groups', user.get_group_info(user.get_unmutual_groups()))
+    user.save_to_json('groups', user.get_group_info(user.get_different_groups()))
 
 
 main()
-
-# ВОПРОС 6: Нашла 2 варианта решения задачи поиска групп, в которых не состоят друзья юзера. В первом варианте
-# (закомменчен) мы ищем соотвествие между членами группы и друзьями юзера через множества. Если пересечения нет,
-# запоминаем id группы. Во втором варианте мы, используя параметры api, получаем только тех участников, кто дружит
-# с юзером.
-# Проблема: в первом варианте получается групп больше, неясно, почему. Во втором варианте групп меньше, но эти группы
-# являются подмножеством множества результатов от первого варианта.
-# Я выбрала второй вариант решения, потому что, на мой взгляд, он более бережливый. Но меня смущает разница в
-# результатах. Буду рада, если вы поможете разобраться.
-
-# unmutual_groups = set()
-
-# ВАРИАНТ 1
-# for group in get_user_groups:
-#     group_params = {
-#         'group_id': group,
-#         'v': '5.73',
-#         'access_token': TOKEN_VK,
-#     }
-#     time.sleep(0.3)
-#     get_group_members = set(requests.get(
-#         'https://api.vk.com/method/groups.getMembers?',
-#         params=group_params
-#     ).json()['response']['items'])
-#
-#     if not get_group_members & get_user_friends:
-#         unmutual_groups.add(str(group))
-
-# ВАРИАНТ 2
-# for group in get_user_groups:
-#     group_params = {
-#         'group_id': group,
-#         'filter': 'friends',
-#         'v': '5.73',
-#         'access_token': TOKEN_VK,
-#     }
-#     time.sleep(0.3)
-#     get_group_members = set(requests.get(
-#         'https://api.vk.com/method/groups.getMembers?',
-#         params=group_params
-#     ).json()['response']['items'])
-#
-#     if not get_group_members:
-#         unmutual_groups.add(str(group))
